8de51122779d0e9d9fa924e8dc29c5c54b3d3b80
Pavel <dvdtygc@gmail.com>
2018-02-10 16:40:06 +0200

    Fix /r to diplsay non sticky only

--- app/core/commands/r.js
+++ app/core/commands/r.js
@@ -8,23 +8,36 @@ module.exports = async commands => {
         return "Wrong syntax.\n\n`/r <subreddit name> <count>`";
     }
 
-    let subreddit = commands[1];
     let count = Number(commands[2]);
-    let url = reddit_url + "r/" + subreddit + ".json";
+
+    if (isNaN(count) || count < 1 || count > 5) {
+        return "Wrong syntax.\n\n`/r <subreddit name> <count (1 to 5)>`";
+    }
+
+    let subreddit = commands[1];
+    let url = reddit_url + "/r/" + subreddit + ".json";
     let articles = [];
     let currentPost = '';
     let link = '';
 
     return await axios.get(url).then(response => {
-        for (let i = 0; i < count;) {
+        for (let i = 0, y = 0; i < count; i++) {
             currentPost = response.data.data.children[i].data;
+
+            if (currentPost.stickied === true) {
+                count++;
+                continue;
+            }
+
+            y++;
             link = currentPost.permalink;
-            articles[i] = '#' + ++i + ': ' + currentPost.title + '\nLink: ' + reddit_url + currentPost.permalink;
+
+            articles[i] = '#' + y + ': ' + currentPost.title + '\nLink: ' + reddit_url + link;
         }
 
-        return "Top " + count + " posts from " + subreddit + ":\n" + articles.join("\n\n");
+        return "**Top " + commands[2] + " posts from " + subreddit + ":**\n" + articles.join("\n");
     }).catch(error => {
-        log(path.dirname(__filename) + path.basename(__filename) + " - " + error);
+        log(path.dirname(__filename) + "/" + path.basename(__filename) + " - " + error);
         return "The subreddit was not found.";
     });
 };
--- config/api/urls/index.js
+++ config/api/urls/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "reddit_url": "https://www.reddit.com/",
+    "reddit_url": "https://www.reddit.com",
     "riot_url": "https://eun1.api.riotgames.com/lol/summoner/v3/summoners/by-name/",
     "wunderground_url": "http://api.wunderground.com/api/"
 };
