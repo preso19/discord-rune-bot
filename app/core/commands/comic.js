5d3ddd2ec0cfe3e16e18c99e8758676ad2280bc3
Pavel <DvDty@users.noreply.github.com>
2018-02-14 18:32:48 +0200

    Update xkcd's max number

--- app/core/commands/comic.js
+++ app/core/commands/comic.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 module.exports = async commands => {
     let min = 1;
-	let max = 1800;
+    let max = 1955;
 
     let random = Math.floor(Math.random() * (max - min + 1)) + min;
 
     let url = "https://xkcd.com/" + random + "/info.0.json";
 
     return await axios.get(url).then(response => {
-    	let decodedResponce = JSON.parse( response );
+    	let decodedResponce = JSON.parse(response);
 
         return decodedResponce->img;
     }).catch(error => {
