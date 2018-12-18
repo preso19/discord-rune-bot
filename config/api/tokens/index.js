dc0873f3f02c970f4b746ebb9933230450ae8ad6
DvDty <dvdtygc@gmail.com>
2018-12-18 18:54:23 +0200

    Fix json

--- config/api/tokens/index.js
+++ config/api/tokens/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     "riot_token": process.env.riot_token,
-    "wunderground_token": process.env.wunderground
+    "wunderground_token": process.env.wunderground,
     "air_token": process.env.air_key
 };
