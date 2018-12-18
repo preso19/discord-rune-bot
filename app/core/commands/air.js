e8813dd18ed95f242f49734d6d145adddb2d7d5d
DvDty <dvdtygc@gmail.com>
2018-12-18 19:01:29 +0200

    Formatting, update npm

--- app/core/commands/air.js
+++ app/core/commands/air.js
@@ -1,33 +1,33 @@
 const axios = require('axios');
-const path = require("path");
+const path = require('path');
 const {air_token} = require('./../../../config/api/tokens/');
 const {air_url} = require('./../../../config/api/urls/');
 const {log} = require('./../handle');
 
 module.exports = async commands => {
     if (commands.length < 2) {
-        return "Wrong syntax.\n\n`/air Country-State-City`";
+        return 'Wrong syntax.\n\n`/air Country-State-City`';
     }
 
     let params = commands[1].split('-');
-    let country = params[0]
-    let state = params[1]
-    let city = params[2]
+    let country = params[0];
+    let state = params[1];
+    let city = params[2];
 
-    if ( params.length < 3 ) {
-        return "Wrong syntax.\n\n`/air Country-State-City`";
+    if (params.length < 3) {
+        return 'Wrong syntax.\n\n`/air Country-State-City`';
     }
 
-    let url = air_url + "city?city=" + city + "&state=" + state + "&country=" + country + "&key=" + air_token;
+    let url = air_url + 'city?city=' + city + '&state=' + state + '&country=' + country + '&key=' + air_token;
 
     return await axios.get(url).then(response => {
         let quality = response.data.current.pollution.aqius;
         let city = response.data.city;
 
-        return "The air quality in " + city + " is " + quality;
+        return 'The air quality in ' + city + ' is ' + quality;
     }).catch(error => {
-        log(path.dirname(__filename) + "/" + path.basename(__filename) + " - " + error);
+        log(path.dirname(__filename) + '/' + path.basename(__filename) + ' - ' + error);
 
-        return "Something has been broken please call Pavel. :OMEGALUL:";
+        return 'Something has been broken please call Pavel. :OMEGALUL:';
     });
 };
